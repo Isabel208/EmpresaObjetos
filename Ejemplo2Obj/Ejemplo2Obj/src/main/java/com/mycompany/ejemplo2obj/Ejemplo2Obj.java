a742e87815fb2459e50a91c12c64b4e680023456
Isabel208 <tovarisabela55@gmail.com>
2024-10-08 22:25:44 -0500

    Add files via upload

---
+++ Ejemplo2Obj/Ejemplo2Obj/src/main/java/com/mycompany/ejemplo2obj/Ejemplo2Obj.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplo2obj;
+
+/**
+ *
+ * @author APRENDIZ
+ */
+import clases.Estudiante;
+import javax.swing.JOptionPane;
+
+public class Ejemplo2Obj {
+
+    public static void main(String[] args) {
+        /*Estudiante estudiante = new Estudiante("Mafe")
+        estudiante.setNotaParcial1(3.5f);
+        estudiante.setNotaParcial2(4.5f);
+        estudiante.calcularNotaFinal();
+        
+        JOptionPane.showMessageDialog(null, "El estudiante " + estudiante.getNombre() + 
+                " obtuvo la nota " + estudiante.getNotaFinal() + 
+                ". Por consiguiente, " + estudiante.obtenerMensaje());
+        
+        Estudiante est2 = new Estudiante("Carlos Rodriguez", 3.5f, 3.5f);
+        est2.calcularNotaFinal();
+        
+        JOptionPane.showMessageDialog(null, "El estudiante " + est2.getNombre() + 
+                " obtuvo la nota " + est2.getNotaFinal() + 
+                ". Por consiguiente, " + est2.obtenerMensaje());*/
+    }
+}
+
+
