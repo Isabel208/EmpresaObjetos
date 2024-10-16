371f35001daa9c9418ff84b95f4e412d472f98a3
Isabel208 <tovarisabela55@gmail.com>
2024-10-16 09:56:02 -0500

    Add files via upload

---
+++ Java 938/Practica.java
@@ -0,0 +1,20 @@
+public class Practica {
+    public static void main(String[] args) {
+        // números enteros
+        int edad = 25;
+
+        // números decimales
+        double precio = 19.99;
+
+        // texto 
+        String nombre = "Emiliano";
+
+        // booleano (verdadero/falso)
+        boolean esEstudiante = true;
+
+        // imprimir variables
+        System.out.println("Edad: " + edad);
+        System.out.println("Precio: " + precio);
+    }
+}
+
